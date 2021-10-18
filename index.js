f26e79b6ee58cca3ce49ca7def4e6934b16abdfe
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-18 15:51:46 -0400

    commit tree cddd10ba91329e4f45fd047b655a1711e3d01593

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+// function mapToNegativize(ray) {
+//     let negNums = []
+//     ray.forEach((posNum) => {
+//         negNums.push(posNum * -1)
+//     })
+//     return negNums;
+// };
+
+function mapToNegativize(ray) {
+    let negNums = [];
+    for (let i = 0; i < ray.length; i++) {
+        negNums.push(ray[i] * -1)
+    };
+    return negNums;
+}
+
+function mapToNoChange(source) {
+    let noChange = []
+    for (let x of source) {
+        noChange.push(x)
+    };
+    return noChange;
+};
+
+function mapToDouble(source) {
+    let doubledValues = []
+    for (let x of source) {
+        doubledValues.push(x * 2)
+    };
+    return doubledValues;
+}
+
+function mapToSquare(source) {
+    let squaredValues = []
+    for (let x of source) {
+        squaredValues.push(x ** 2)
+    };
+    return squaredValues;
+};
+
+function reduceToTotal(source, start = 0) {
+    let total = start;
+    for (let x of source) {
+        total += x;
+    };
+    return total;
+};
+
+function reduceToAllTrue (source) {
+    for (let i = 0; i < source.length; i++) {
+       if (!source[i]) return false
+    };
+    return true
+};
+
+function reduceToAnyTrue (source) {
+    for (let i = 0; i < source.length; i++) {
+       if (source[i]) return true
+    };
+    return false;
+};
